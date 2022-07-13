358aa4c367660683b1ca89742feeae755bd83d92
RicardoCortesV <iudokinfo@gmail.com>
2022-07-12 21:53:36 -0500

    Pequenios arreglos

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-let comprasSuper =["Leche, huevo, carnes, frijol, arroz"];
-for (let i = 0;i<comprasSuper.length;i++){
-	console.log( "Esta es la lista de los artìculos: " + comprasSuper[i]);
+let comprasSuper = ["Leche, huevo, carnes, frijol, arroz"];
+for (let i = 0; i < comprasSuper.length; i++) {
+	console.log("Esta es la lista de los artìculos: " + comprasSuper[i]);
 }
